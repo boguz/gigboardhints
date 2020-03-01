9062cb6f49482db171c7756da2ba30df648a3f47
Boguz <luis_bento@hotmail.com>
2020-03-01 11:50:57 +0100

    [TASK] Add presents 0 to 50

---
+++ src/data/presets.js
@@ -0,0 +1,308 @@
+export const fxs = [
+    {
+        preset: "001 How does it feel",
+        band: "Bob Dylan",
+        song: "Like a rolling stone",
+        confirmed: false
+    },
+    {
+        preset: "002 Personal Guru",
+        band: "Depeche Mode",
+        song: "Personal Jesus",
+        confirmed: false
+    },
+    {
+        preset: "",
+        band: "",
+        song: "",
+        confirmed: false
+    },
+    {
+        preset: "003 Modern Plexi",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "004 Food Fight",
+        band: "Foo Fighters",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "005 Sweet Home Bro",
+        band: "Lynyrd Skynyrd",
+        song: "Sweet Home Alabama",
+        confirmed: false
+    },
+    {
+        preset: "006 Passing Lane",
+        band: "The Eagles",
+        song: "Life in the fast lane",
+        confirmed: false
+    },
+    {
+        preset: "007 Shreddin Cult",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "008 Sandman",
+        band: "Metallica",
+        song: "Enter Sandman",
+        confirmed: false
+    },
+    {
+        preset: "009 Like a Bell",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "010 Clean Shivers",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "011 More Cowbell",
+        band: "Mountain",
+        song: "Mississippi Queen",
+        confirmed: false
+    },
+    {
+        preset: "012 Crazy Ted",
+        band: "Ted Nugent",
+        song: "Cat Scratch Fever",
+        confirmed: false
+    },
+    {
+        preset: "013 Industrial",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "014 x Roads",
+        band: "Cream",
+        song: "Crossroads",
+        confirmed: false
+    },
+    {
+        preset: "015 Jim",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "016 Brown 800",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "017 Purple Bike",
+        band: "Prince",
+        song: "Purple Rain (Rhythm)",
+        confirmed: false
+    },
+    {
+        preset: "018 Purple Solo",
+        band: "Prince",
+        song: "Purple Rain (Solo)",
+        confirmed: false
+    },
+    {
+        preset: "019 Heart Pwner",
+        band: "Yes",
+        song: "Barracuda",
+        confirmed: false
+    },
+    {
+        preset: "020 Scared Robot",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "021 Run away fast",
+        band: "Pink Floyd",
+        song: "Run like hell",
+        confirmed: false
+    },
+    {
+        preset: "022 Ghost in the sky",
+        band: "Normann Greenbaum",
+        song: "Spirit in the sky",
+        confirmed: false
+    },
+    {
+        preset: "023 Swella",
+        band: "-",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "024 Only to be",
+        band: "U2",
+        song: "Still haven't found",
+        confirmed: false
+    },
+    {
+        preset: "025 Daddy's New Bag",
+        band: "james Brown",
+        song: "Papa's got a brand new bag",
+        confirmed: false
+    },
+    {
+        preset: "026 Juju Eyeball",
+        band: "The Beatles",
+        song: "Come together",
+        confirmed: false
+    },
+    {
+        preset: "027 Insain Train",
+        band: "Ozzy Osbourne",
+        song: "Crazy Train",
+        confirmed: false
+    },
+    {
+        preset: "028 Gimme a Kiss",
+        band: "Prince",
+        song: "Kiss (Rhythm)",
+        confirmed: false
+    },
+    {
+        preset: "029 Gimme a Solo",
+        band: "Prince",
+        song: "Kiss (Solo)",
+        confirmed: false
+    },
+    {
+        preset: "030 White Cliffs",
+        band: "Eric Johnson",
+        song: "White Cliffs of Dover",
+        confirmed: false
+    },
+    {
+        preset: "031 Beastly Stone",
+        band: "The Rolling Stones",
+        song: "Beast of Burden",
+        confirmed: false
+    },
+    {
+        preset: "032 Shelter",
+        band: "The Rolling Stones",
+        song: "Gimme Shelter (Intro)",
+        confirmed: false
+    },
+    {
+        preset: "033 Route 666",
+        band: "AC/DC",
+        song: "Highway to Hell",
+        confirmed: false
+    },
+    {
+        preset: "034 Heart Riff",
+        band: "Neil Young",
+        song: "Heart of Gold",
+        confirmed: false
+    },
+    {
+        preset: "035 My Aim if true",
+        band: "Elvis Costello",
+        song: "My aim is true",
+        confirmed: false
+    },
+    {
+        preset: "036 Generation Me",
+        band: "The Who",
+        song: "My Generation",
+        confirmed: false
+    },
+    {
+        preset: "037 Don't cry",
+        band: "Bob Marley",
+        song: "No woman no cry",
+        confirmed: false
+    },
+    {
+        preset: "038 Surf",
+        band: "The Ventures",
+        song: "-",
+        confirmed: false
+    },
+    {
+        preset: "039 Shot in Reno",
+        band: "Johnny Cash",
+        song: "Folsom Prison Blues",
+        confirmed: false
+    },
+    {
+        preset: "040 Nuthin Dig",
+        band: "Elvis Costello",
+        song: "Hound Dog",
+        confirmed: false
+    },
+    {
+        preset: "041 Black Lab",
+        band: "Led Zeppelin",
+        song: "Black Dog",
+        confirmed: false
+    },
+    {
+        preset: "042 Dusty Queen",
+        band: "Queen",
+        song: "Another one bites the dust",
+        confirmed: false
+    },
+    {
+        preset: "043 Bohemia Solo",
+        band: "Queen",
+        song: "Bohemian Rhapsody",
+        confirmed: false
+    },
+    {
+        preset: "044 U Will be rocked",
+        band: "Queen",
+        song: "We will rock you",
+        confirmed: false
+    },
+    {
+        preset: "045 Show don't lie",
+        band: "Eric Clapton",
+        song: "Cocaine",
+        confirmed: false
+    },
+    {
+        preset: "046 Georges Wife",
+        band: "Eric Clapton",
+        song: "Layla",
+        confirmed: false
+    },
+    {
+        preset: "047 Sunshine",
+        band: "Cream",
+        song: "Sunshine of your life",
+        confirmed: false
+    },
+    {
+        preset: "048 Black Curtains",
+        band: "Cream",
+        song: "White Room",
+        confirmed: false
+    },
+    {
+        preset: "049 Tonight Won",
+        band: "Eric Clapton",
+        song: "Wonderful Tonight",
+        confirmed: false
+    },
+    {
+        preset: "050 Swamp Born",
+        band: "Creedence ClearWater Revival",
+        song: "Born on the Bayou",
+        confirmed: false
+    },
+]
